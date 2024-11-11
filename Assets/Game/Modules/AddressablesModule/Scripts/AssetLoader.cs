f143da3ffd583274f8ee17767e13c6c1889c0613
casey-gregor <36679045+casey-gregor@users.noreply.github.com>
2024-11-12 00:48:43 +0400

    created 2 loaders for Assets and for Scenes
    AssetLoader - can load and unload any types of assets.
    SceneLoader - can load and unload scenes.

---
+++ Assets/Game/Modules/AddressablesModule/Scripts/AssetLoader.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Game.Modules.AddressablesModule.Scripts
+{
+    public sealed class AssetLoader<T>
+    {
+        private readonly Dictionary<string, AsyncOperationHandle<T>> _handles = new();
+        public async Task<T> LoadAsset(string assetName)
+        {
+            if (_handles.TryGetValue(assetName, out var handle))
+            {
+                return handle.Result;
+            }
+
+            AsyncOperationHandle<T> newHandle = Addressables.LoadAssetAsync<T>(assetName);
+            await newHandle.Task;
+
+            if (newHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                _handles[assetName] = newHandle;
+                return newHandle.Result;
+            }
+
+            Debug.LogError($"Failed to load asset with key: {assetName}. No location found.");
+            return default;
+        }
+
+        public void Release(string assetName)
+        {
+            if (_handles.TryGetValue(assetName, out var handle))
+            {
+                Addressables.Release(handle);
+                _handles.Remove(assetName);
+            }
+        }
+
+        public void ReleaseAll()
+        {
+            foreach (var handle in _handles.Values)
+            {
+                Addressables.Release(handle);
+            }
+            _handles.Clear();
+        }
+
+        public bool Contains(string assetName) => _handles.ContainsKey(assetName);
+    }
+}
