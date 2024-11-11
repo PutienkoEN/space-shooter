63fed464b7c309938096a61ce545d7d01b59ec98
casey-gregor <36679045+casey-gregor@users.noreply.github.com>
2024-11-11 17:18:00 +0400

    fixed folders and asmdef conflict
    moved images and materials into Background module folder.
    added separate asmdef for SettingsManager.
    added separate zenject installer for SettingsManager.

--- Assets/Game/Modules/Background/Zenject/BackgroundInstaller.cs
+++ Assets/Game/Modules/Background/Zenject/BackgroundInstaller.cs
@@ -1,15 +1,13 @@
 ﻿using SpaceShooter.Background;
-using SpaceShooter;
 
 namespace Zenject
 {
-    public class SceneInstaller : MonoInstaller
+    public class BackgroundInstaller : MonoInstaller
     {
         public override void InstallBindings()
         {
             Container.Bind<BackgroundController>().AsSingle().NonLazy();
             Container.BindInterfacesAndSelfTo<BackgroundView>().FromComponentInHierarchy().AsSingle().NonLazy();
-            Container.Bind<SettingsManager>().FromComponentInHierarchy().AsSingle().NonLazy();
         }
     }
 }
---
+++ Assets/Game/Modules/SettingsManager/Zenject/SettingsInstaller.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+
+namespace SpaceShooter.Zenject
+{
+    public class SettingsInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<SettingsManager>().FromComponentInHierarchy().AsSingle().NonLazy();
+        }
+
+    }
+}
