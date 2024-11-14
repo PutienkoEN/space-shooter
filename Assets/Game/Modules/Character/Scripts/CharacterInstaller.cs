bdcddabc00a6c75aeaf5d547a890632a82f4213e
Evgenii <justmysimplemail@gmail.com>
2024-11-14 08:04:50 +0300

    Extract move component separately.

---
+++ Assets/Game/Modules/Character/Scripts/CharacterInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Input;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Modules.Character
+{
+    public class CharacterInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject character;
+        [SerializeField] private float speed;
+
+        public override void InstallBindings()
+        {
+            var transform = character.GetComponent<Transform>();
+
+            Container
+                .Bind<CharacterMoveComponent>()
+                .AsSingle()
+                .WithArguments(transform, speed);
+        }
+    }
+}
--- Assets/Game/Modules/Character/Scripts/CharacterMoveComponent.cs
+++ Assets/Game/Modules/Character/Scripts/CharacterMoveComponent.cs
@@ -1,18 +1,27 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Zenject;
 
 namespace Input
 {
     [Serializable]
     public class CharacterMoveComponent
     {
-        [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private Transform transform;
         [SerializeField] private float speed;
+
+        [Inject]
+        public CharacterMoveComponent(Transform transform, float speed)
+        {
+            this.transform = transform;
+            this.speed = speed;
+        }
 
         public void Move(Vector3 targetPosition)
         {
-            var moveTowards = Vector3.MoveTowards(rigidbody.position, targetPosition, Time.fixedDeltaTime * speed);
-            rigidbody.MovePosition(moveTowards);
+            var moveTowards = Vector3.MoveTowards(transform.position, targetPosition, Time.fixedDeltaTime * speed);
+            transform.Translate(moveTowards);
         }
     }
 }
