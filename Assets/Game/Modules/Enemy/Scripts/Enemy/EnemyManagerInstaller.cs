a95942d5d5b996cd4ec7d607aa112baf15e3abe3
Evgenii <justmysimplemail@gmail.com>
2024-11-28 03:07:49 +0300

    Cleanup project

---
+++ Assets/Game/Modules/Enemy/Scripts/Enemy/EnemyManagerInstaller.cs
@@ -0,0 +1,29 @@
+﻿using SpaceShooter.Game.LifeCycle.Common;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Modules.Enemy.Scripts
+{
+    public class EnemyManagerInstaller : GameModuleInstaller
+    {
+        [SerializeField] private Transform worldTransform;
+
+        public override void Install(DiContainer container)
+        {
+            container
+                .BindFactory<EnemyView, Vector3, Quaternion, EnemyView, EnemyViewFactory>()
+                .WithFactoryArguments(worldTransform)
+                .AsSingle();
+
+            container
+                .BindFactory<Vector3, Quaternion, EnemyData, EnemyEntity, EnemyEntity.Factory>()
+                .FromSubContainerResolve()
+                .ByInstaller<EnemyInstaller>()
+                .AsSingle();
+
+            container
+                .BindInterfacesAndSelfTo<EnemyManager>()
+                .AsSingle();
+        }
+    }
+}
---
+++ Assets/Game/Modules/Level/Scripts/LevelManagerInstaller.cs
@@ -0,0 +1,29 @@
+﻿using SpaceShooter.Game.LifeCycle.Common;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Modules.Enemy.Scripts
+{
+    public class LevelManagerInstaller : GameModuleInstaller
+    {
+        [SerializeField] private GameLevelConfig levelConfig;
+
+        public override void Install(DiContainer container)
+        {
+            container
+                .Bind<LevelManager>()
+                .AsSingle();
+
+            container
+                .Bind<GameLevelData>()
+                .FromInstance(levelConfig.GetData())
+                .AsSingle();
+
+            container
+                .Bind<DebugLevelManager>()
+                .FromComponentInHierarchy()
+                .AsSingle()
+                .NonLazy();
+        }
+    }
+}
