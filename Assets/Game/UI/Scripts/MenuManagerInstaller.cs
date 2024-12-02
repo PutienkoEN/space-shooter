2a1e068cfb245bc2ee89fd51c04dd57fa6be356e
casey-gregor <36679045+casey-gregor@users.noreply.github.com>
2024-12-02 15:27:15 +0400

    added MainMenuManager and Installer for it. added MenuScene.unity with background and canvas and particles.

---
+++ Assets/Game/UI/Scripts/MenuManagerInstaller.cs
@@ -0,0 +1,16 @@
+﻿using SpaceShooter.Game.LifeCycle.Common;
+using UnityEngine;
+using Zenject;
+
+namespace Game.UI.Scripts
+{
+    public class MenuManagerInstaller : GameModuleInstaller
+    {
+        [SerializeField] private Animator animator;
+        
+        public override void Install(DiContainer container)
+        {
+            container.BindInterfacesTo<MainMenuManager>().AsSingle().WithArguments(animator);
+        }
+    }
+}
