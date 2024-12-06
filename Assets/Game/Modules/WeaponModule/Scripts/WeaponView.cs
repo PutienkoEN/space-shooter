3fbb315d3ce58f16232dc71ddb4931bf2d567e77
Evgenii <justmysimplemail@gmail.com>
2024-12-07 01:46:57 +0300

    Add logic for shoot sound

--- Assets/Game/Modules/WeaponModule/Scripts/WeaponView.cs
+++ Assets/Game/Modules/WeaponModule/Scripts/WeaponView.cs
@@ -4,16 +4,23 @@ namespace Game.Modules.ShootingModule.Scripts
 {
     public class WeaponView : MonoBehaviour, IWeaponView
     {
+        public AudioSource audioSource;
         public Transform[] firePoints;
 
         public Transform[] GetFirePoints()
         {
             return firePoints;
         }
+
+        public void PlayShootSound()
+        {
+            audioSource.Play();
+        }
     }
 
     public interface IWeaponView
     {
+        public void PlayShootSound();
         public Transform[] GetFirePoints();
     }
 }
