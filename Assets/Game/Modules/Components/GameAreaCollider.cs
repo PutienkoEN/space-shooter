9b24fbcbe0c7aa4eb4c3783774a0d4dcc94ab5b8
casey-gregor <36679045+casey-gregor@users.noreply.github.com>
2024-12-14 14:14:42 +0400

    creating gamearea collider. not finished

---
+++ Assets/Game/Modules/Components/GameAreaCollider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Modules.Components
+{
+    public class GameAreaCollider : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            var obj = other.GetComponentInParent<IGameAreaEnter>();
+            obj?.OnEnter();
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var obj = other.GetComponentInParent<IGameAreaExit>();
+            obj?.OnExit();
+        }
+    }
+
+    public interface IGameAreaEnter
+    {
+        public void OnEnter();
+    }
+
+    public interface IGameAreaExit
+    {
+        public void OnExit();
+    }
+}
